ae360d1cb5b451c289d00ea3d166974c8e8acdf1
daniel mishael <dmishael91@gmail.com>
2019-10-16 14:50:15 -0400

    need to revisit refactoring this codebase

--- client/src/components/AddBookingForm.js
+++ client/src/components/AddBookingForm.js
@@ -1,53 +1,53 @@
-import React, { Component } from 'react';
-import axios from 'axios'
+// import React, { Component } from 'react';
+// import axios from 'axios'
 
 
-class AddBookingForm extends Component {
+// class AddBookingForm extends Component {
 
-    state = {
-        booking: {
-            userName: ''
-        }
-    }
+//     state = {
+//         booking: {
+//             userName: ''
+//         }
+//     }
 
-    handleChange = (event) => {
-        const newBooking = { ...this.state.booking }
-        console.log("posts here", this.state.booking)
-        newBooking[event.target.name] = event.target.value
-        this.setState({ booking: newBooking })
+//     handleChange = (event) => {
+//         const newBooking = { ...this.state.booking }
+//         console.log("posts here", this.state.booking)
+//         newBooking[event.target.name] = event.target.value
+//         this.setState({ booking: newBooking })
 
-    }
+//     }
 
-    handleSubmit = (event) => {
-        event.preventDefault()
-        const booking = this.state.booking
-        const postId = this.props.match.params.id
-        axios.post(`/api/posts/${postId}`, booking)
-            .then(() => this.props.history.goBack())
+//     handleSubmit = (event) => {
+//         event.preventDefault()
+//         const booking = this.state.booking
+//         const postId = this.props.match.params.id
+//         axios.post(`/api/posts/${postId}`, booking)
+//             .then(() => this.props.history.goBack())
 
-    }
-
-
+//     }
 
 
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <div>
-                        <input
-                            type="text"
-                            placeholder="User Name - Gallons Committed"
-                            name="userName"
-                            value={this.state.booking.userName}
-                            onChange={this.handleChange}
-                        />
-                    </div>
-                    <button>Submit</button>
-                </form>
-            </div>
-        );
-    }
-}
 
-export default AddBookingForm;
+
+//     render() {
+//         return (
+//             <div>
+//                 <form onSubmit={this.handleSubmit}>
+//                     <div>
+//                         <input
+//                             type="text"
+//                             placeholder="User Name - Gallons Committed"
+//                             name="userName"
+//                             value={this.state.booking.userName}
+//                             onChange={this.handleChange}
+//                         />
+//                     </div>
+//                     <button>Submit</button>
+//                 </form>
+//             </div>
+//         );
+//     }
+// }
+
+// export default AddBookingForm;
