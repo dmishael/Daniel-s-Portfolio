a42b01a4300935c6944e8c3e0c6a9b3841549ae4
daniel mishael <dmishael91@gmail.com>
2019-10-16 14:40:13 -0400

    refactored About page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-// import axios from 'axios'
-// import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Navbar from "../components/Nav";
 
@@ -13,12 +11,7 @@ const Body = styled.body`
   margin-left: 15%;
   margin-right: 10%;
 `;
-// const Title = styled.h2`
-//     float: right;
-//     padding-top: 6%;
-//     margin-right: 47%;
-//     font-family: 'sans simplifica','times new roman';
-//     `
+
 
 const IMG = styled.img`
   display: block;
@@ -36,81 +29,8 @@ const P = styled.p`
 `;
 
 
-// const Button = styled.a`
-// text-decoration: none;
-// background: #ffec99;
-// text-align: "center";
-// display: "inline";
-// color: black;
-// font-size: 1em;
-// margin: 3%;
-// padding: 0.25em 1em;
-// border: .8px solid black;
-// border-radius: 20px;
-// `
-
-// BOOP, one day you'll see this comment
-// from cameron
-
-// const List = styled.div`
-// text-decoration: none;
-// font-weight: bold;
-// color: "black";
-// display: inline-block;
-// `
-// const StyledLink = styled(Link)`
-// text-decoration: none;
-// background: #ffec99;
-// text-align: "center";
-// color: black;
-// font-size: 1em;
-// margin: 3%;
-// padding: 0.25em 1em;
-// border: .8px solid black;
-// border-radius: 20px;
-// display: inline-block;
-// `
-// const Edit = styled.div`
-// margin-left: 60%;
-// `
-// const Nav = styled.nav`
-// font-family: Courier New;
-// width: 100%;
-// background-color: #45616C;
-// text-align: right;
-// `
-// const NavLink = styled(Link)`
-// text-decoration: none;
-// background: #ffec99;
-// border-radius: 20px;
-// padding: 1%;
-// margin: 20px;
-// color: black;
-// `
-
 class Users extends Component {
 
-    // state = {
-    //     users: []
-    // }
-
-    // componentDidMount() {
-    //     this.getAllUsers()
-    // }
-
-    // getAllUsers = () => {
-    //     axios.get(`api/users`).then((res) => {
-    //         this.setState({ users: res.data })
-
-    //     })
-    // }
-
-    // deleteIdea = (event, userId) => {
-    //     event.preventDefault()
-    //     axios.delete(`/api/users/${userId}`).then((res) => {
-    //         this.getAllUsers()
-    //     })
-    // }
 
     render() {
 
