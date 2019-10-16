d1371370c37b67071fc634fccc3226f2bae7e314
daniel mishael <dmishael91@gmail.com>
2019-10-16 14:41:58 -0400

    refactored bookingform and addpost

--- client/src/components/AddPost.js
+++ client/src/components/AddPost.js
@@ -1,72 +1,72 @@
-import React, { Component } from 'react';
-import axios from 'axios'
+// import React, { Component } from 'react';
+// import axios from 'axios'
 
 
-class AddPostForm extends Component {
+// class AddPostForm extends Component {
 
-    state = {
-        post: {
-            pickupDate: '',
-            composter: '',
-            gallonsNeeded: ''
+//     state = {
+//         post: {
+//             pickupDate: '',
+//             composter: '',
+//             gallonsNeeded: ''
 
-        }
-    }
+//         }
+//     }
 
-    handleChange = (event) => {
-        const newPost = { ...this.state.post }
-        newPost[event.target.name] = event.target.value
-        this.setState({ post: newPost })
-    }
+//     handleChange = (event) => {
+//         const newPost = { ...this.state.post }
+//         newPost[event.target.name] = event.target.value
+//         this.setState({ post: newPost })
+//     }
 
-    handleSubmit = (event) => {
-        event.preventDefault()
-        const input = this.state.post
-        const userId = this.props.match.params.id
-        axios.post(`/api/users/${userId}/posts`, input).then((res) => {
+//     handleSubmit = (event) => {
+//         event.preventDefault()
+//         const input = this.state.post
+//         const userId = this.props.match.params.id
+//         axios.post(`/api/users/${userId}/posts`, input).then((res) => {
 
-            this.props.history.goBack()
-        })
+//             this.props.history.goBack()
+//         })
 
-    }
+//     }
 
 
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <div>
-                        <input
-                            type="text"
-                            placeholder="Pick Up Date"
-                            name="pickupDate"
-                            value={this.state.post.pickupDate}
-                            onChange={this.handleChange}
-                        />
-                    </div>
-                    <div>
-                        <input
-                            type="text"
-                            placeholder="Composter Name"
-                            name="composter"
-                            value={this.state.post.composter}
-                            onChange={this.handleChange}
-                        />
-                    </div>
-                    <div>
-                        <input
-                            type="text"
-                            placeholder="Gallons Needed"
-                            name="gallonsNeeded"
-                            value={this.state.post.gallonsNeeded}
-                            onChange={this.handleChange}
-                        />
-                    </div>
-                    <button>Submit</button>
-                </form>
-            </div>
-        );
-    }
-}
+//     render() {
+//         return (
+//             <div>
+//                 <form onSubmit={this.handleSubmit}>
+//                     <div>
+//                         <input
+//                             type="text"
+//                             placeholder="Pick Up Date"
+//                             name="pickupDate"
+//                             value={this.state.post.pickupDate}
+//                             onChange={this.handleChange}
+//                         />
+//                     </div>
+//                     <div>
+//                         <input
+//                             type="text"
+//                             placeholder="Composter Name"
+//                             name="composter"
+//                             value={this.state.post.composter}
+//                             onChange={this.handleChange}
+//                         />
+//                     </div>
+//                     <div>
+//                         <input
+//                             type="text"
+//                             placeholder="Gallons Needed"
+//                             name="gallonsNeeded"
+//                             value={this.state.post.gallonsNeeded}
+//                             onChange={this.handleChange}
+//                         />
+//                     </div>
+//                     <button>Submit</button>
+//                 </form>
+//             </div>
+//         );
+//     }
+// }
 
-export default AddPostForm;
+// export default AddPostForm;
